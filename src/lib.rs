56dfd7f649b76f65c86ab61770186db77a0e1f7b
jD91mZM2 <me@krake.one>
2018-09-22 16:20:30 +0200

    feat: Implement a fill() function

--- src/lib.rs
+++ src/lib.rs
@@ -2,12 +2,12 @@
 
 extern crate num_traits;
 
-use core::mem;
-use num_traits::{PrimInt, FromPrimitive};
+use core::{mem, ops::{Bound, RangeBounds}};
+use num_traits::{Bounded, PrimInt, FromPrimitive, ToPrimitive};
 
 pub trait BitArray: Default + Clone + Copy {
     /// The item type this array holds
-    type Item: Default + PrimInt + FromPrimitive;
+    type Item: Default + PrimInt + FromPrimitive + ToPrimitive;
     /// Returns how many elements this array can hold
     fn len() -> usize;
     /// Access the element at a specified index.
@@ -25,7 +25,7 @@ pub trait BitArray: Default + Clone + Copy {
 macro_rules! impl_arrays {
     ($($len:expr),*) => {
         $(
-            impl<T: Default + PrimInt + FromPrimitive> BitArray for [T; $len] {
+            impl<T: Default + PrimInt + FromPrimitive + ToPrimitive> BitArray for [T; $len] {
                 type Item = T;
 
                 fn len() -> usize { $len }
@@ -76,9 +76,9 @@ impl<T: BitArray> BitSet<T> {
         Self::default()
     }
     /// Returns the capacity of the set, in other words how many bits it can
-    /// hold. This may very well overflow if the size or length is too big, but
-    /// if you're making that big allocations you probably got bigger things to
-    /// worry about.
+    /// hold. This function may very well overflow if the size or length is too
+    /// big, but if you're making that big allocations you probably got bigger
+    /// things to worry about.
     pub fn capacity() -> usize {
         T::len() * Self::item_size()
     }
@@ -88,15 +88,12 @@ impl<T: BitArray> BitSet<T> {
     }
     /// Converts a u64 to a T::Item
     fn from(i: u64) -> T::Item {
-        FromPrimitive::from_u64(i).unwrap()
-    }
-
-    /// Empty the set, clearing all bits
-    pub fn clear(&mut self) {
-        for i in 0..T::len() {
-            *self.inner.get_mut(i) = Default::default();
+        match FromPrimitive::from_u64(i & T::Item::max_value().to_u64().unwrap()) {
+            Some(i) => i,
+            None => panic!("couldn't convert {} to the generic type", i)
         }
     }
+
     /// Enable the value `bit` in the set.
     /// If `bit` is already enabled this is a no-op.
     pub fn insert(&mut self, bit: usize) {
@@ -106,7 +103,7 @@ impl<T: BitArray> BitSet<T> {
     /// See the struct level documentation for notes on panicking.
     pub fn try_insert(&mut self, bit: usize) -> bool {
         if bit >= Self::capacity() {
-            return false
+            return false;
         }
         let index = bit / Self::item_size();
         *self.inner.get_mut(index) =
@@ -122,7 +119,7 @@ impl<T: BitArray> BitSet<T> {
     /// See the struct level documentation for notes on panicking.
     pub fn try_remove(&mut self, bit: usize) -> bool {
         if bit >= Self::capacity() {
-            return false
+            return false;
         }
         let index = bit / Self::item_size();
         *self.inner.get_mut(index) =
@@ -159,6 +156,76 @@ impl<T: BitArray> BitSet<T> {
             total += self.inner.get(i).count_zeros();
         }
         total
+    }
+
+    /// Disable all bits in a range.
+    pub fn clear(&mut self) {
+        for i in 0..T::len() {
+            *self.inner.get_mut(i) = Default::default();
+        }
+    }
+    /// Set all bits in a range.
+    /// `fill(.., false)` is effectively the same as `clear()`.
+    ///
+    /// # Panics
+    /// Panics if the start or end bounds are more than the capacity.
+    pub fn fill<R: RangeBounds<usize>>(&mut self, range: R, on: bool) {
+        let mut start = match range.start_bound() {
+            Bound::Unbounded => 0,
+            Bound::Included(&i) => {
+                assert!(i <= Self::capacity(), "start bound is too big for capacity");
+                i
+            },
+            Bound::Excluded(&i) => {
+                assert!(i + 1 <= Self::capacity(), "start bound is too big for capacity");
+                i + 1
+            }
+        };
+        let end = match range.end_bound() {
+            Bound::Unbounded => Self::capacity(),
+            Bound::Included(0) => return,
+            Bound::Included(&i) => {
+                assert!(i - 1 <= Self::capacity(), "end bound is too big for capacity");
+                i - 1
+            },
+            Bound::Excluded(&i) => {
+                assert!(i <= Self::capacity(), "end bound is too big for capacity");
+                i
+            }
+        };
+
+        if start >= end {
+            return;
+        }
+
+        let end_first = start - (start % Self::item_size()) + Self::item_size();
+        if start % Self::item_size() != 0 || end < end_first {
+            // Unaligned write to either the end or the start of next integer
+            let end_first = end_first.min(end);
+            // println!("Doing initial unaligned from {} to {}", start, end_first);
+            for bit in start..end_first {
+                if on { self.insert(bit); } else { self.remove(bit); }
+            }
+
+            if end == end_first {
+                return;
+            }
+
+            start = end_first + 1;
+        }
+
+        // Fast way to fill all bits in all integers: Just set them to the min/max value.
+        let start_last = end - (end % Self::item_size());
+        // println!("Doing aligned from {} to {}", start, start_last);
+        for i in start / Self::item_size()..start_last / Self::item_size() {
+            *self.inner.get_mut(i) = if on { Bounded::max_value() } else { Default::default() };
+        }
+
+        // Unaligned write to the end
+        // println!("Doing unaligned from {} to {}", start_last, end);
+        for bit in start_last..end {
+            if on { self.insert(bit); } else { self.remove(bit); }
+        }
     }
 }
 
@@ -216,14 +283,14 @@ mod tests {
     }
     #[test]
     fn remove() {
-        let mut set = BitSet64::new();
-        set.insert(35);
-        set.insert(42);
-        assert!(set.contains(35));
-        assert!(set.contains(42));
-        set.remove(42);
-        assert!(set.contains(35));
-        assert!(!set.contains(42));
+        let mut set = BitSet32::new();
+        set.insert(12);
+        set.insert(17);
+        assert!(set.contains(12));
+        assert!(set.contains(17));
+        set.remove(17);
+        assert!(set.contains(12));
+        assert!(!set.contains(17));
     }
     #[test]
     fn clear() {
@@ -243,5 +310,48 @@ mod tests {
         set.insert(7);
         assert_eq!(set.count_ones(), 2);
         assert_eq!(set.count_zeros(), 8 - 2);
+    }
+    #[test]
+    fn fill() {
+        // Care must be taken when changing the `range` function, as this test
+        // won't detect if it actually does as many aligned writes as it can.
+
+        let mut set = BitSet::<[u8; 2]>::new();
+
+        // Aligned
+        set.fill(.., true);
+        for i in 0..16 {
+            assert!(set.contains(i));
+        }
+
+        // println!("---");
+
+        // Within the same int
+        set.clear();
+        set.fill(1..3, true);
+        assert!(!set.contains(0));
+        assert!(set.contains(1));
+        assert!(set.contains(2));
+        assert!(!set.contains(3));
+
+        // println!("---");
+
+        // Unaligned end
+        set.clear();
+        set.fill(8..10, true);
+        assert!(!set.contains(7));
+        assert!(set.contains(8));
+        assert!(set.contains(9));
+        assert!(!set.contains(10));
+
+        // println!("---");
+
+        // Unaligned start
+        set.clear();
+        set.fill(3..16, true);
+        assert!(!set.contains(2));
+        for i in 3..16 {
+            assert!(set.contains(i));
+        }
     }
 }
